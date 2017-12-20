7c54a8c3b3af12f8d6a3edf9ac072b6862174f99
ingielek <ingielewicz.p@gmail.com>
2017-12-20 18:15:59 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-
+import {Grid, Jumbotron} from 'react-bootstrap'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div >
+        <Grid>
+                <Jumbotron>
+                 <h1>Localee</h1>
+                <p>Show us Your point.</p>
+                </Jumbotron>
+        </Grid>
 
       </div>
     );
